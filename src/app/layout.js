9e980fd1bcb7f495c30b8a1f1b16802446eae723
salauddin <salauddin@hasan.com>
2026-04-25 02:07:06 +0600

    feat: add Poppins font using next/font and apply globally

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,19 @@
-import Header from "@/components/Header";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "600", "700"], 
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en"
+    <html
+     lang="en" 
      data-theme="light"
-      className={`h-full antialiased`}>
-      <body className="min-h-full flex flex-col">
-       
+      className="h-full antialiased">
+      <body className={`${poppins.className} min-h-full flex flex-col`}>
         <main>{children}</main>
       </body>
     </html>
